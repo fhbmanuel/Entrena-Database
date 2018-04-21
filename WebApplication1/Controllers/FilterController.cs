1e67bb09a0ee3e16eb43ebaaa75e5f88103ed5a4
fhbmanuel <fhbmanuel@gmail.com>
2018-04-21 02:17:28 -0700

    Almost perfect. Only to do a final revision.

--- WebApplication1/Controllers/FilterController.cs
+++ WebApplication1/Controllers/FilterController.cs
@@ -17,17 +17,18 @@ namespace WebApplication1.Controllers
         // GET: Filter
         public ActionResult Index()
         {
-            string query = "select Activity_Name, Starting_Date, Participant_Name, Participant_Last_Name, " +
+            string query = "select Activity_Name, Starting_Date, Facilitator_Name, Participant_Name, Participant_Last_Name, " +
                 "Gender_Name, Participant_Date_Of_Birth, Sub_Product_Name, Product_Name, " +
                 "Country_Name, Province_Name, Location_Name, Activity_ID, Participant_ID " +
-                "from((((((((Activities a join Activities_Participation ap on ap.ActivityID = a.Activity_ID) " +
+                "from(((((((((Activities a join Activities_Participation ap on ap.ActivityID = a.Activity_ID) " +
                 "join Participants p on p.Participant_ID = ap.ParticipantID) " +
                 "join Genders g on g.Gender_ID = p.GenderID) " +
                 "join Sub_Products sp on sp.Sub_Product_ID = a.SubProductID) " +
                 "join Products pr on pr.Product_ID = sp.ProductID) " +
                 "join Locations l on l.Location_ID = a.LocationID) " +
                 "join Provinces pv on pv.Province_ID = l.ProvinceID) " +
-                "join Countries c on c.Country_ID = pv.CountryID)";
+                "join Countries c on c.Country_ID = pv.CountryID)" +
+                "join Facilitators f on f.Facilitator_ID = a.Facilitator1 or f.Facilitator_ID = a.Facilitator2 or f.Facilitator_ID = a.Facilitator3)";
             SqlConnection conn = new SqlConnection(@"Data Source=MANUELHDEZ\MSSQLSERVER01;Initial Catalog=Entrena_QL_Database;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
             DataTable dt = new DataTable();
             SqlCommand objCommand = new SqlCommand(query, conn);
